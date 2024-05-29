23f45982a15196e7814fa21c9ad1ee4c21314719
chansamangrathana <chansamangrathana1607@gmail.com>
2024-05-29 08:56:20 -0700

    course detail

--- app/student/courses/page.tsx
+++ app/student/courses/page.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import CourseDetail from '@/components/ui/coursedetail/CourseDetail'
 
 export default function page() {
   return (
-    <div>course page</div>
+
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <CourseDetail />
+    </div>
   )
 }
