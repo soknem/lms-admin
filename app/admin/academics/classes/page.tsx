9de38869da9cee4311adfeb8ddcd007f5888d1c9
YengSokroza <yengsokroza@gmail.com>
2024-05-30 10:41:21 +0700

    generation page and class page in academics

--- app/admin/academics/classes/page.tsx
+++ app/admin/academics/classes/page.tsx
@@ -2,22 +2,14 @@ import React from 'react'
 import {DataTable} from "./data-table"
 import {columns} from "./columns"
 
-async function getGenerations(): Promise<GenerationType[]> {
-  const res = await fetch(
-    'https://6656cd809f970b3b36c69232.mockapi.io/api/v1/generations'
-  )
-  const data = await res.json()
-  
-  console.log("data from page: ",data);
-  return data
-}
+
 
 export default function page() {
   return (
     <section className='py-9'>
         <div className='container'>
           <h1 className='mb-6 text-3xl font-bold text-primary'>Generation</h1>
-          <DataTable columns={columns} data={data} />
+          
         </div>
 
        
