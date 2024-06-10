526e723120d3970672632253f50c3a7fea5284c6
chansamangrathana <chansamangrathana1607@gmail.com>
2024-06-09 22:48:29 -0700

    edit instructor and admin report

--- app/admin/(admin-dashboard)/reports/page.tsx
+++ app/admin/(admin-dashboard)/reports/page.tsx
@@ -6,19 +6,22 @@ import lectures from "@/app/admin/(admin-dashboard)/academics/lectures/data/lect
 import { Filter } from "@/components/instructorComponent/reports/attendence/Filter";
 import { LectureColumns } from "@/components/adminComponent/academics/lectures/LectureColumns";
 import { FilterAdmin } from "@/components/adminComponent/reports/Filter";
+import ReportComponent from "@/components/adminComponent/reports/student/ReportComponent";
 
 export default async function Report() {
   const payData = await getPayment();
   const data: LectureType[] = lectures;
   return (
-    <main className="p-9">
+    <main className="p-9 ">
       <h2 className="text-4xl text-lms-primary font-bold">Reports</h2>
       <div className="ml-[900px] ">
-              <FilterAdmin columns={LectureColumns} data={data} />
-
+        <FilterAdmin columns={LectureColumns} data={data} />
       </div>
 
       <TabComponent />
+      <div className="mt-2">
+        <ReportComponent />
+      </div>
     </main>
   );
 }
