983eacbc26d393b3a6478d446ddd675af02d56c7
chansamangrathana <chansamangrathana1607@gmail.com>
2024-06-12 10:25:45 -0700

    instructor and admin report

--- components/adminComponent/faculties/subject/columns.tsx
+++ components/adminComponent/faculties/subject/columns.tsx
@@ -8,7 +8,7 @@ import { MoreHorizontal, ArrowUpDown } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useState, useEffect, ChangeEvent, MouseEvent } from "react";
 import { StatusOption, SubjectType } from "@/lib/types/admin/faculty";
-import ActionsCell from "./SubActionCell";
+import ActionsCell from "@/components/adminComponent/faculties/subject/SubActionCell";
 //. @ts-ignore
 
 const TableCell = ({ getValue, row, column, table }: any) => {
