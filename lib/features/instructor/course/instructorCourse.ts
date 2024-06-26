6be0d74294be77d212599d1fc5c66ad8d63d4715
chansamangrathana <chansamangrathana1607@gmail.com>
2024-06-26 13:51:34 -0700

    pull

---
+++ lib/features/instructor/course/instructorCourse.ts
@@ -0,0 +1,14 @@
+// studentApi.ts
+import { istadLmsApi } from "@/lib/api";
+
+export const instructorCourseApi = istadLmsApi.injectEndpoints({
+    endpoints: (builder) => ({
+        getInstructorCourse: builder.query<any, void>({
+            query: () => `/instructors/courses`,
+        }),
+    }),
+});
+
+export const {
+    useGetInstructorCourseQuery,
+} = instructorCourseApi;
