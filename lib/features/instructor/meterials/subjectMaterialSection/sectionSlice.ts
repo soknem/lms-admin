100a3935ffb770f8b78e527802748efbe8c15125
Panha <sanhpanha3003@gmail.com>
2024-07-18 23:42:23 +0700

    Fixed error after merging

--- lib/features/instructor/meterials/subjectMaterialSection/sectionSlice.ts
+++ lib/features/instructor/meterials/subjectMaterialSection/sectionSlice.ts
@@ -1,6 +1,6 @@
 import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 import type {RootState} from "@/lib/store";
-import {SectionType} from "@/lib/types/instructor/materials";
+import {SectionType} from "@/lib/types/admin/materials";
 
 type SectionState = {
     sections: SectionType[];
